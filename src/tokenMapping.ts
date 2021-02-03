5110f5bee061076e4439cff7374df18736797713
nigepujie74 <1030532788@qq.com>
2021-02-03 20:42:06 +0800

    LXX

--- src/tokenMapping.ts
+++ src/tokenMapping.ts
@@ -1,22 +1,28 @@
-import { NewGravatar, UpdatedGravatar } from '../generated/Gravity/Gravity'
-import { Gravatar } from '../generated/schema'
+import { GetCashCall,TotalBorrowsCall,
+  CallSupplyRatePerBlock,BorrowRatePerBlockCall} from '../generated/Dai/Dai'
+import { GetCash,TotalBorrow,SupplyRatePerBlock,BorrowRatePerBlock } from '../generated/schema'
 
-export function handleNewGravatar(event: NewGravatar): void {
-  let gravatar = new Gravatar(event.params.id.toHex())
-  gravatar.owner = event.params.owner
-  gravatar.displayName = event.params.displayName
-  gravatar.imageUrl = event.params.imageUrl
-  gravatar.save()
+
+export function handleGetCash(call: GetCashCall): void {
+  let getCash = new GetCash()
+  getCash.cash = call.outputs//代改
+  getCash.save()
 }
 
-export function handleUpdatedGravatar(event: UpdatedGravatar): void {
-  let id = event.params.id.toHex()
-  let gravatar = Gravatar.load(id)
-  if (gravatar == null) {
-    gravatar = new Gravatar(id)
-  }
-  gravatar.owner = event.params.owner
-  gravatar.displayName = event.params.displayName
-  gravatar.imageUrl = event.params.imageUrl
-  gravatar.save()
+export function handleGetTotalBorrows(call: TotalBorrowsCall): void {
+  let totalBorrow = new TotalBorrow()
+  totalBorrow.borrow = call.outputs//代改
+  totalBorrow.save()
+}
+
+export function handleSupplyRatePerBlock(call: CallSupplyRatePerBlock): void {
+  let supplyRatePerBlock = new SupplyRatePerBlock()
+  supplyRatePerBlock.rate = call.outputs//代改
+  supplyRatePerBlock.save()
+}
+
+export function handleBorrowRatePerBlock(call: BorrowRatePerBlockCall): void {
+  let borrowRatePerBlock = new BorrowRatePerBlock()
+  borrowRatePerBlock.rate = call.outputs//代改
+  borrowRatePerBlock.save()
 }
