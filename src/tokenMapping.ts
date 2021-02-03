f2b3a3589fd0571dafd3f295182ea33178642b6b
nigepujie74 <1030532788@qq.com>
2021-02-03 21:30:35 +0800

    lxx

--- src/tokenMapping.ts
+++ src/tokenMapping.ts
@@ -32,7 +32,7 @@ import { GetCash,TotalBorrow,SupplyRatePerBlock,BorrowRatePerBlock } from '../ge
 
 
 export function handleTotalBorrowsCurrent(call: TotalBorrowsCurrentCall): void {
-  let id = call.params.id.toHex()
+  let id = call.totalBorrow.id.toHex()
   let totalBorrow = new TotalBorrow(id)
   totalBorrow.borrow = call.outputs//代改
   totalBorrow.save()
