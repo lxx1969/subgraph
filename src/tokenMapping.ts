82ff66fad066ca2ca927ec52d8988c2e743ae9eb
nigepujie74 <1030532788@qq.com>
2021-02-03 21:01:51 +0800

    LXX

--- src/tokenMapping.ts
+++ src/tokenMapping.ts
@@ -1,28 +1,38 @@
-import { GetCashCall,TotalBorrowsCall,
-  CallSupplyRatePerBlock,BorrowRatePerBlockCall} from '../generated/Dai/Dai'
+// import { GetCashCall,TotalBorrowsCall,
+//   CallSupplyRatePerBlock,BorrowRatePerBlockCall} from '../generated/Dai/Dai'
+// import { GetCash,TotalBorrow,SupplyRatePerBlock,BorrowRatePerBlock } from '../generated/schema'
+
+
+// export function handleGetCash(call: GetCashCall): void {
+//   let getCash = new GetCash()
+//   getCash.cash = call.outputs//代改
+//   getCash.save()
+// }
+
+// export function handleGetTotalBorrows(call: TotalBorrowsCall): void {
+//   let totalBorrow = new TotalBorrow()
+//   totalBorrow.borrow = call.outputs//代改
+//   totalBorrow.save()
+// }
+
+// export function handleSupplyRatePerBlock(call: CallSupplyRatePerBlock): void {
+//   let supplyRatePerBlock = new SupplyRatePerBlock()
+//   supplyRatePerBlock.rate = call.outputs//代改
+//   supplyRatePerBlock.save()
+// }
+
+// export function handleBorrowRatePerBlock(call: BorrowRatePerBlockCall): void {
+//   let borrowRatePerBlock = new BorrowRatePerBlock()
+//   borrowRatePerBlock.rate = call.outputs//代改
+//   borrowRatePerBlock.save()
+// }
+
+import {TotalBorrowsCurrentCall} from '../generated/Dai/Dai'
 import { GetCash,TotalBorrow,SupplyRatePerBlock,BorrowRatePerBlock } from '../generated/schema'
 
 
-export function handleGetCash(call: GetCashCall): void {
-  let getCash = new GetCash()
-  getCash.cash = call.outputs//代改
-  getCash.save()
-}
-
-export function handleGetTotalBorrows(call: TotalBorrowsCall): void {
+export function handleTotalBorrowsCurrent(call: TotalBorrowsCurrentCall): void {
   let totalBorrow = new TotalBorrow()
   totalBorrow.borrow = call.outputs//代改
   totalBorrow.save()
-}
-
-export function handleSupplyRatePerBlock(call: CallSupplyRatePerBlock): void {
-  let supplyRatePerBlock = new SupplyRatePerBlock()
-  supplyRatePerBlock.rate = call.outputs//代改
-  supplyRatePerBlock.save()
-}
-
-export function handleBorrowRatePerBlock(call: BorrowRatePerBlockCall): void {
-  let borrowRatePerBlock = new BorrowRatePerBlock()
-  borrowRatePerBlock.rate = call.outputs//代改
-  borrowRatePerBlock.save()
 }
