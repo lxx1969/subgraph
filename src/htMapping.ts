135f33a159065afed3307a21c8f57fa7f471dc9d
nigepujie74 <1030532788@qq.com>
2021-02-04 19:11:03 +0800

    LXX

--- src/htMapping.ts
+++ src/htMapping.ts
@@ -1,11 +1,20 @@
 import { Borrow } from '../generated/HT/HT'
 import { HtBorrowImpl } from '../generated/schema'
 
-export function handleBorrows(event: Borrow): void {
-  let htBorrowImpl = new HtBorrowImpl("HtBorrow");
-  htBorrowImpl.totalBorrows = event.params.totalBorrows
+export function handleBorrows(event: Borrow,): void {
+    
+
+    let htBorrowImpl   = HtBorrowImpl.load("HtBorrow")
+    if(htBorrowImpl == null){
+ htBorrowImpl = new HtBorrowImpl("HtBorrow");
+  
+    }
+    htBorrowImpl.totalBorrows = event.params.totalBorrows
   htBorrowImpl.borrower = event.params.borrower
   htBorrowImpl.accountBorrows = event.params.accountBorrows
   htBorrowImpl.borrowAmount = event.params.borrowAmount
   htBorrowImpl.save()
+
+
+
 }
