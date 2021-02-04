3aa7fe4a7417c55b4effe38375bbfc467f3d7c9e
nigepujie74 <1030532788@qq.com>
2021-02-04 20:58:35 +0800

    LXX

--- src/htMapping.ts
+++ src/htMapping.ts
@@ -1,4 +1,4 @@
-import { Borrow,InitializeCall,HT} from '../generated/HT/HT'
+import { Borrow,InitializeCall,HT,Mint} from '../generated/HT/HT'
 import { HtBorrowImpl} from '../generated/schema'
 
 export function handleBorrows(event: Borrow,call:InitializeCall,HT:HT): void {
@@ -20,7 +20,33 @@ export function handleBorrows(event: Borrow,call:InitializeCall,HT:HT): void {
   htBorrowImpl.supplyRatePerBlock = HT.supplyRatePerBlock()
   htBorrowImpl.borrowRatePerBlock = HT.borrowRatePerBlock()
 
+
+  htBorrowImpl.save()
+
+
+}
+
+
+export function handleMint(event: Mint,call:InitializeCall,HT:HT): void {
+    
+
+    let htBorrowImpl   = HtBorrowImpl.load("HtMint")
+    if(htBorrowImpl == null){
+ htBorrowImpl = new HtBorrowImpl("HtMint");
   
+    }
+//     htBorrowImpl.totalBorrows = event.params.totalBorrows
+//   htBorrowImpl.borrower = event.params.borrower
+//   htBorrowImpl.accountBorrows = event.params.accountBorrows
+//   htBorrowImpl.borrowAmount = event.params.borrowAmount
+//   htBorrowImpl.decimals = call.inputs.decimals_
+
+//   htBorrowImpl.accountMint = HT.balanceOfUnderlying(htBorrowImpl.borrower)
+  htBorrowImpl.totalMint = HT.getCash()
+  htBorrowImpl.supplyRatePerBlock = HT.supplyRatePerBlock()
+  htBorrowImpl.borrowRatePerBlock = HT.borrowRatePerBlock()
+
+
   htBorrowImpl.save()
 
 
