6f35f8feb7b4d644df4d2fa5446ccc078dd327ba
nigepujie74 <1030532788@qq.com>
2021-02-04 20:15:17 +0800

    LXX

--- src/htMapping.ts
+++ src/htMapping.ts
@@ -1,7 +1,7 @@
-import { Borrow } from '../generated/HT/HT'
-import { HtBorrowImpl } from '../generated/schema'
+import { Borrow,InitializeCall,HT} from '../generated/HT/HT'
+import { HtBorrowImpl} from '../generated/schema'
 
-export function handleBorrows(event: Borrow,): void {
+export function handleBorrows(event: Borrow,call:InitializeCall,HT:HT): void {
     
 
     let htBorrowImpl   = HtBorrowImpl.load("HtBorrow")
@@ -13,8 +13,18 @@ export function handleBorrows(event: Borrow,): void {
   htBorrowImpl.borrower = event.params.borrower
   htBorrowImpl.accountBorrows = event.params.accountBorrows
   htBorrowImpl.borrowAmount = event.params.borrowAmount
+  htBorrowImpl.decimals = call.inputs.decimals_
+
+  htBorrowImpl.accountMint = HT.balanceOfUnderlying(htBorrowImpl.borrower)
+  htBorrowImpl.totalMint = HT.getCash()
+  htBorrowImpl.supplyRatePerBlock = HT.supplyRatePerBlock()
+  htBorrowImpl.borrowRatePerBlock = HT.borrowRatePerBlock()
+
+  
   htBorrowImpl.save()
 
 
-
 }
+
+
+
