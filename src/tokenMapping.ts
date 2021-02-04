c3c6e2e0968d718d8617ac2a3464700ce078ebd2
nigepujie74 <1030532788@qq.com>
2021-02-04 15:45:33 +0800

    LXX

--- src/tokenMapping.ts
+++ src/tokenMapping.ts
@@ -46,9 +46,8 @@ export function handleBorrows(event: Borrow,call:InitializeCall): void {
   if (borrow == null) {
     borrow = new BorrowImpl("borrow")
   }
-
- 
-  borrow.decimals =  parseInt(call.inputs.decimals_)
+  
+  borrow.decimals =  call.inputs.decimals_
   borrow.borrowAmount = event.params.borrowAmount
   borrow.accountBorrows = event.params.accountBorrows
   borrow.totalBorrows = event.params.totalBorrows
