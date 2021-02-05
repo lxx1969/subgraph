aff1aecb2077ab325840f18c265d0b248790710b
nigepujie74 <1030532788@qq.com>
2021-02-05 14:38:55 +0800

    LXX

--- src/htMapping.ts
+++ src/htMapping.ts
@@ -10,16 +10,17 @@ export function handleBorrows(event: Borrow): void {
   htBorrowImpl.borrower = event.params.borrower
   htBorrowImpl.accountBorrows = event.params.accountBorrows
   htBorrowImpl.borrowAmount = event.params.borrowAmount
-  log.info('htBorrowImpl is htBorrowImpl.accountBorrows',[htBorrowImpl.accountBorrows])
-  log.info('htBorrowImpl is htBorrowImpl.borrower',[htBorrowImpl.borrower])
-  log.info('htBorowImpl is htBorrowImpl.borrowAmount',[htBorrowImpl.borrowAmount])
-  log.info('htBorrowImpl is  htBorrowImpl.totalBorrows',[ htBorrowImpl.totalBorrows]);
+  log.info('event',[event.toString()])
+  log.info('htBorrowImpl is htBorrowImpl.accountBorrows',[htBorrowImpl.accountBorrows.toString()])
+  log.info('htBorrowImpl is htBorrowImpl.borrower',""+[htBorrowImpl.borrower.toString()])
+  log.info('htBorowImpl is htBorrowImpl.borrowAmount',""+[htBorrowImpl.borrowAmount.toString()])
+  log.info('htBorrowImpl is  htBorrowImpl.totalBorrows',""+[ htBorrowImpl.totalBorrows.toString()]);
   
   let address = Address.fromString('0xddc822c72e6CC10Af98De2D53cC04dAeb4a5336e')
   let Ht = HT.bind(address)
 
   let decimals = BigInt.fromI32(Ht.decimals())
-  log.info('HTimpl decimals is',[decimals]);
+  log.info('HTimpl decimals is',"" +[decimals.toString()]);
   
   htBorrowImpl.save()
   
