c72156f0d920aa9dfae23189ba97dc5ea611a162
nigepujie74 <1030532788@qq.com>
2021-02-05 14:08:20 +0800

    LXX

--- src/htMapping.ts
+++ src/htMapping.ts
@@ -1,6 +1,6 @@
 import { Borrow,InitializeCall,HT,Mint} from '../generated/HT/HT'
 import { HtBorrowImpl,HtMintImpl} from '../generated/schema'
-import{log} from '@graphprotocol/graph-ts'
+import{log,BigInt,Address} from '@graphprotocol/graph-ts'
 export function handleBorrows(event: Borrow): void {
     
 
@@ -11,9 +11,12 @@ export function handleBorrows(event: Borrow): void {
   htBorrowImpl.accountBorrows = event.params.accountBorrows
   htBorrowImpl.borrowAmount = event.params.borrowAmount
   let array = [htBorrowImpl.accountBorrows,htBorrowImpl.borrower,htBorrowImpl.accountBorrows,htBorrowImpl.totalBorrows]
-  log.info('htBorrowImpl is htBorrowImpl.accountBorrows,htBorrowImpl.borrower,htBorrowImpl.accountBorrows,htBorrowImpl.totalBorrows',array)
-  let Ht = HT.bind(0xddc822c72e6CC10Af98De2D53cC04dAeb4a5336e)
-  let decimals = Ht.decimals()
+  log.info('htBorrowImpl is htBorrowImpl.accountBorrows,htBorrowImpl.borrower,htBorrowImpl.accountBorrows,htBorrowImpl.totalBorrows',[array[0],array[1],array[2],array[3]])
+
+  let address = Address.fromString('0xddc822c72e6CC10Af98De2D53cC04dAeb4a5336e')
+  let Ht = HT.bind(address)
+
+  let decimals = BigInt.fromI32(Ht.decimals())
   log.info('HTimpl decimals is',[decimals]);
   
   htBorrowImpl.save()
