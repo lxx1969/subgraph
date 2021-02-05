67ac092b8dfad8486f77851e4f7662d634a2de78
nigepujie74 <1030532788@qq.com>
2021-02-05 11:58:35 +0800

    LXX

--- src/htMapping.ts
+++ src/htMapping.ts
@@ -1,58 +1,58 @@
-import { Borrow,InitializeCall,HT,Mint} from '../generated/HT/HT'
-import { HtBorrowImpl} from '../generated/schema'
+// import { Borrow,InitializeCall,HT,Mint} from '../generated/HT/HT'
+// import { HtBorrowImpl} from '../generated/schema'
 
-export function handleBorrows(event: Borrow,call:InitializeCall,HT:HT): void {
+// export function handleBorrows(event: Borrow,call:InitializeCall,HT:HT): void {
     
 
-    // let htBorrowImpl   = HtBorrowImpl.load("HtBorrow")
-    // if(htBorrowImpl == null){
- let htBorrowImpl = new HtBorrowImpl("HtBorrow");
+//     // let htBorrowImpl   = HtBorrowImpl.load("HtBorrow")
+//     // if(htBorrowImpl == null){
+//  let htBorrowImpl = new HtBorrowImpl("HtBorrow");
   
-    // }
-    htBorrowImpl.totalBorrows = event.params.totalBorrows
-  htBorrowImpl.borrower = event.params.borrower
-  htBorrowImpl.accountBorrows = event.params.accountBorrows
-  htBorrowImpl.borrowAmount = event.params.borrowAmount
-//   htBorrowImpl.decimals = call.inputs.decimals_
+//     // }
+//     htBorrowImpl.totalBorrows = event.params.totalBorrows
+//   htBorrowImpl.borrower = event.params.borrower
+//   htBorrowImpl.accountBorrows = event.params.accountBorrows
+//   htBorrowImpl.borrowAmount = event.params.borrowAmount
+// //   htBorrowImpl.decimals = call.inputs.decimals_
    
  
-//   htBorrowImpl.decimals = 18
-//   htBorrowImpl.accountMint = HT.balanceOfUnderlying(htBorrowImpl.borrower)
+// //   htBorrowImpl.decimals = 18
+// //   htBorrowImpl.accountMint = HT.balanceOfUnderlying(htBorrowImpl.borrower)
+// //   htBorrowImpl.totalMint = HT.getCash()
+// //   htBorrowImpl.supplyRatePerBlock = HT.supplyRatePerBlock()
+// //   htBorrowImpl.borrowRatePerBlock = HT.borrowRatePerBlock()
+
+
+//   htBorrowImpl.save()
+  
+
+// }
+
+
+// export function handleMint(event: Mint,call:InitializeCall,HT:HT): void {
+    
+
+//     let htBorrowImpl   = HtBorrowImpl.load("HtMint")
+//     if(htBorrowImpl == null){
+//  htBorrowImpl = new HtBorrowImpl("HtMint");
+  
+//     }
+// //     htBorrowImpl.totalBorrows = event.params.totalBorrows
+// //   htBorrowImpl.borrower = event.params.borrower
+// //   htBorrowImpl.accountBorrows = event.params.accountBorrows
+// //   htBorrowImpl.borrowAmount = event.params.borrowAmount
+// //   htBorrowImpl.decimals = call.inputs.decimals_
+
+// //   htBorrowImpl.accountMint = HT.balanceOfUnderlying(htBorrowImpl.borrower)
 //   htBorrowImpl.totalMint = HT.getCash()
 //   htBorrowImpl.supplyRatePerBlock = HT.supplyRatePerBlock()
 //   htBorrowImpl.borrowRatePerBlock = HT.borrowRatePerBlock()
 
 
-  htBorrowImpl.save()
-  
-
-}
+//   htBorrowImpl.save()
 
 
-export function handleMint(event: Mint,call:InitializeCall,HT:HT): void {
-    
-
-    let htBorrowImpl   = HtBorrowImpl.load("HtMint")
-    if(htBorrowImpl == null){
- htBorrowImpl = new HtBorrowImpl("HtMint");
-  
-    }
-//     htBorrowImpl.totalBorrows = event.params.totalBorrows
-//   htBorrowImpl.borrower = event.params.borrower
-//   htBorrowImpl.accountBorrows = event.params.accountBorrows
-//   htBorrowImpl.borrowAmount = event.params.borrowAmount
-//   htBorrowImpl.decimals = call.inputs.decimals_
-
-//   htBorrowImpl.accountMint = HT.balanceOfUnderlying(htBorrowImpl.borrower)
-  htBorrowImpl.totalMint = HT.getCash()
-  htBorrowImpl.supplyRatePerBlock = HT.supplyRatePerBlock()
-  htBorrowImpl.borrowRatePerBlock = HT.borrowRatePerBlock()
-
-
-  htBorrowImpl.save()
-
-
-}
+// }
 
 
 
